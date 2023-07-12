a5a17955f3db08e6195cf02d6f5f1275dda6064c
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-07-12 19:43:36 +0200

    Added Dockerfile

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ rm -rf /root/.cache
 ln -s /runpod-volume/.cache /root/.cache
 
 echo "Starting RunPod Handler"
-source /workspace/Real-ESRGAN/venv/bin/activate
-mv /rp_handler.py /workspace/Real-ESRGAN/rp_handler.py
-cd /workspace/Real-ESRGAN
+export PYTHONUNBUFFERED=1
+source /workspace/runpod-worker-real-esrgan/venv/bin/activate
+cd /workspace/runpod-worker-real-esrgan
 python -u rp_handler.py
