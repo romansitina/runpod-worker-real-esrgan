b7b84a1bfd1ad1ebf2e340d0793188c23da323c9
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-07-12 18:06:38 +0200

    Load pre-downloaded models from RunPod workspace

--- rp_handler.py
+++ rp_handler.py
@@ -11,6 +11,8 @@ from realesrgan import RealESRGANer
 
 GPU_ID = 0
 TMP_PATH = '/tmp/upscaler'
+MODELS_PATH = '/workspace/ESRGAN/models'
+GFPGAN_MODEL_PATH = '/workspace/GFPGAN/models/GFPGANv1.3.pth'
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -58,19 +60,16 @@ def upscale(
     if model_name == 'RealESRGAN_x4plus':  # x4 RRDBNet model
         model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
         netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth']
     elif model_name == 'RealESRNet_x4plus':  # x4 RRDBNet model
         model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
         netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.1/RealESRNet_x4plus.pth']
     elif model_name == 'RealESRGAN_x4plus_anime_6B':  # x4 RRDBNet model with 6 blocks
         model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4)
         netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth']
     elif model_name == 'RealESRGAN_x2plus':  # x2 RRDBNet model
         model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
         netscale = 2
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth']
+    # TODO: Implement these
     # elif model_name == 'realesr-animevideov3':  # x4 VGG-style model (XS size)
     #     model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
     #     netscale = 4
@@ -86,28 +85,18 @@ def upscale(
         raise KeyError(f'Unsupported model: {model_name}')
 
     # determine model paths
-    model_path = os.path.join('weights', model_name + '.pth')
+    model_path = os.path.join(MODELS_PATH, model_name + '.pth')
 
     if not os.path.isfile(model_path):
-        ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-        for url in file_url:
-            # model_path will be updated
-            model_path = load_file_from_url(
-                url=url,
-                model_dir=os.path.join(ROOT_DIR, 'weights'),
-                progress=True,
-                file_name=None
-            )
+        raise Exception(f'Could not find model: {model_path}')
 
     # use dni to control the denoise strength
     dni_weight = None
-    if model_name == 'realesr-general-x4v3' and denoise_strength != 1:
-        wdn_model_path = model_path.replace('realesr-general-x4v3', 'realesr-general-wdn-x4v3')
-        model_path = [model_path, wdn_model_path]
-        dni_weight = [denoise_strength, 1 - denoise_strength]
+    # if model_name == 'realesr-general-x4v3' and denoise_strength != 1:
+    #     wdn_model_path = model_path.replace('realesr-general-x4v3', 'realesr-general-wdn-x4v3')
+    #     model_path = [model_path, wdn_model_path]
+    #     dni_weight = [denoise_strength, 1 - denoise_strength]
 
-    # restorer
     upsampler = RealESRGANer(
         scale=netscale,
         model_path=model_path,
@@ -123,7 +112,7 @@ def upscale(
     if face_enhance:  # Use GFPGAN for face enhancement
         from gfpgan import GFPGANer
         face_enhancer = GFPGANer(
-            model_path='https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth',
+            model_path=GFPGAN_MODEL_PATH,
             upscale=outscale,
             arch='clean',
             channel_multiplier=2,
