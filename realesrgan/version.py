0a525040510b75aa04e222c2475eaa456d89ffe3
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-07-12 17:36:03 +0200

    Updated gitsha

--- realesrgan/version.py
+++ realesrgan/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Wed Jul 12 13:55:51 2023
+# TIME: Wed Jul 12 17:35:47 2023
 __version__ = '0.3.0'
-__gitsha__ = 'bd2024d'
+__gitsha__ = 'af290b9'
 version_info = (0, 3, 0)
